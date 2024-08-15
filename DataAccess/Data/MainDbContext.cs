649ea6c3c105a9478dfc7c7e3500195432fdbe9d
islam alabassy <islamabassy226@gmail.com>
2024-08-15 22:16:27 +0300

    Adding products in customer views

--- DataAccess/Data/MainDbContext.cs
+++ DataAccess/Data/MainDbContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Models;
 using Models.Models;
 
 namespace DataAccess
 {
-    public class MainDbContext : DbContext
+    public class MainDbContext : IdentityDbContext
     {
         private readonly DbContextOptions<MainDbContext> options;
 
